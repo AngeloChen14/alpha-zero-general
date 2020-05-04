eb5e92f530856ab4ea4aa491b34aa84f5d5c8a4c
AngeloChen14 <angelochen@live.cn>
2020-05-05 02:16:42 +0800

    little fix

--- pit.py
+++ pit.py
@@ -26,8 +26,7 @@ hp = HumanPickGamePlayer(g).play
 
 # nnet players
 n1 = NNet(g)
-# n1.load_checkpoint('./pretrained_models/pickgame/keras','4x4-best.pth.tar')
-n1.load_checkpoint('./temp', 'temp.pth.tar')
+n1.load_checkpoint('./pretrained_models/pickgame/keras','4x4-best.pth.tar')
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -39,7 +38,7 @@ else:
         player2 = rp
     else:
         n2 = NNet(g)
-        n2.load_checkpoint('./temp', 'temp.pth.tar')
+        n2.load_checkpoint('./temp', 'best.pth.tar')
         args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
         mcts2 = MCTS(g, n2, args2)
         n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
